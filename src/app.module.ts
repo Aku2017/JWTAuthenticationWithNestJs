22be5c267a163f12cc1eddc9cdabc5d6f03a5dd7
Michael <akumichael2011@gmail.com>
2023-09-15 19:12:04 +0100

    updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [AuthModule, UserModule, PrismaModule],
   controllers: [AppController],
 })
 export class AppModule {}
